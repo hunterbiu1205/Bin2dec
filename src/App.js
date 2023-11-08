437bcbfb35e13e74921fe989e140dba202568061
Hunter Dong <75303443+hunterbiu1205@users.noreply.github.com>
2023-11-08 11:17:34 +0000

    Update 4 files

--- src/App.js
+++ src/App.js
@@ -5,28 +5,41 @@ import React, { useState } from "react";
 export default function App() {
   const [inputValue, setInputValue] = useState("");
   const [decimalValue, setDecimalValue] = useState(null);
+  const [warning, setWarning] = useState("");
 
   const handleInputChange = (e) => {
-    setInputValue(e.target.value);
+    const userInput = e.target.value;
+    setInputValue(userInput);
+
+    const isValid = /^[01]*$/.test(userInput);
+
+    setWarning(isValid ? "" : "Input should contain only 0s and 1s");
+    setDecimalValue(isValid ? toDecimalConverter(userInput) : null);
   };
 
-  const convertToDecimal = () => {
-    const binary = inputValue.split("").reverse();
+  const toDecimalConverter = (v) => {
+    const binary = v.split("").reverse();
     let decimal = 0;
 
     for (let i = 0; i < binary.length; i++) {
       decimal += binary[i] * Math.pow(2, i);
     }
 
-    setDecimalValue(decimal);
+    return decimal;
   };
-
   return (
     <div className="App">
-      <input type="text" value={inputValue} onChange={handleInputChange} />
-      <h1>test</h1>
-      <button onClick={convertToDecimal}>Convert</button>
+      <h1>Bin2Dec</h1>
+      <p>Enter a binary number, get a decimal conversion.</p>
+      <input
+        type="text"
+        maxlength="20"
+        value={inputValue}
+        onChange={handleInputChange}
+      />
+      {warning && <p className="warning">{warning}</p>}
       {decimalValue !== null && <p>Decimal value: {decimalValue}</p>}
     </div>
   );
 }
+// Test update commit
