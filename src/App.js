239bd1cc87f77e1abf7e6d38656a0e303e5878cb
Hunter Dong <75303443+hunterbiu1205@users.noreply.github.com>
2023-11-08 15:21:57 +1030

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import "./styles.css";
+
+import React, { useState } from "react";
+
+export default function App() {
+  const [inputValue, setInputValue] = useState("");
+  const [decimalValue, setDecimalValue] = useState(null);
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const convertToDecimal = () => {
+    const binary = inputValue.split("").reverse();
+    let decimal = 0;
+
+    for (let i = 0; i < binary.length; i++) {
+      decimal += binary[i] * Math.pow(2, i);
+    }
+
+    setDecimalValue(decimal);
+  };
+
+  return (
+    <div className="App">
+      <input type="text" value={inputValue} onChange={handleInputChange} />
+
+      <button onClick={convertToDecimal}>Convert</button>
+      {decimalValue !== null && <p>Decimal value: {decimalValue}</p>}
+    </div>
+  );
+}
